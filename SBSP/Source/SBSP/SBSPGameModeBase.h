eae3bc0f6ad921cdd380e8abdf4848bdbdd1cb1e
Rajan117 <77441871+Rajan117@users.noreply.github.com>
2024-05-21 09:53:51 +0100

    Created Project

---
+++ SBSP/Source/SBSP/SBSPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SBSPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SBSP_API ASBSPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
