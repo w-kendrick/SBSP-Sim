399221f3b382a27b1f41f2d2da2e25e17443b65b
Rajan117 <77441871+Rajan117@users.noreply.github.com>
2024-05-22 09:41:29 +0100

    Added Actors For Harbour And Tiles

---
+++ SBSP/Source/SBSP/HexGrid/HexTile.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HexTile.h"
+
+
+// Sets default values
+AHexTile::AHexTile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AHexTile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHexTile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
